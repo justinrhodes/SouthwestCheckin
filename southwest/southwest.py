006ddf1f5dd8d4c072abc89a13a8c283da72e03c
Ryan Schoen <trschoen@gmail.com>
2021-09-11 13:46:40 -0700

    Added workaround for southwest's new ee30zvqlwf header scheme

--- southwest/southwest.py
+++ southwest/southwest.py
@@ -29,7 +29,18 @@ class Reservation():
 
         USER_EXPERIENCE_KEY = str(uuid.uuid1()).upper()
         # Pulled from proxying the Southwest iOS App
-        return {'Host': 'mobile.southwest.com', 'Content-Type': 'application/json', 'X-API-Key': API_KEY, 'X-User-Experience-Id': USER_EXPERIENCE_KEY, 'Accept': '*/*', 'X-Channel-ID': 'MWEB'}
+        headers = {'Host': 'mobile.southwest.com', 'Content-Type': 'application/json', 'X-API-Key': API_KEY, 'X-User-Experience-Id': USER_EXPERIENCE_KEY, 'Accept': '*/*', 'X-Channel-ID': 'MWEB'}
+		
+        headers_file = open('headers.txt', 'r')
+        lines = headers_file.readlines()
+        for line in lines:
+            if ': ' in line:
+                (name, value) = line.strip().split(': ')
+                name = name.lower()
+                if 'ee30zvqlwf' in name:
+                    headers[name] = value
+		
+        return headers
 
     # You might ask yourself, "Why the hell does this exist?"
     # Basically, there sometimes appears a "hiccup" in Southwest where things
